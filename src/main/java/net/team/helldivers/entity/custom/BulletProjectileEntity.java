af269126897a8dc2b8674c57e21c5a889702e33c
XeO3-git <lgsky8@gmail.com>
2025-07-28 21:32:56 -0400

    added particles when a bullet hits a block

--- src/main/java/net/team/helldivers/entity/custom/BulletProjectileEntity.java
+++ src/main/java/net/team/helldivers/entity/custom/BulletProjectileEntity.java
@@ -2,29 +2,28 @@ package net.team.helldivers.entity.custom;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.BlockParticleOption;
 import net.minecraft.core.particles.DustParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.animal.IronGolem;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.GlassBlock;
 import net.minecraft.world.level.block.IronBarsBlock;
-import net.minecraft.world.level.block.StainedGlassPaneBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.*;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.phys.Vec3;
 import net.team.helldivers.block.custom.BotContactMineBlock;
 import net.team.helldivers.entity.ModEntities;
 
@@ -34,6 +33,8 @@ public class BulletProjectileEntity extends AbstractArrow {
     private int lifetime = 0;
     private boolean isShotgun;
     private boolean isAmr;
+    private int maxLife = 40;
+    private int maxShotgunLife = 10;
 
     public BulletProjectileEntity(EntityType<? extends AbstractArrow> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
@@ -81,7 +82,18 @@ public class BulletProjectileEntity extends AbstractArrow {
                 entity.hurt(this.level().damageSources().explosion(null), 12.5F);
             });
         }
-        this.discard();
+        if(this.level().isClientSide){
+            //add particles on hit
+            BlockParticleOption blockParticle = new BlockParticleOption(ParticleTypes.BLOCK, block);
+            for(int i=0;i<25;i++){
+                System.out.println("hi");
+                double rand1 = Math.random()*plusmin();
+                double rand2 = Math.random()*plusmin();          
+                double rand3 = Math.random()*plusmin();            
+                this.level().addParticle(blockParticle,this.getX(), this.getY(), this.getZ(),rand1, rand2, rand3);
+            }
+        }
+       this.lifetime = maxLife-2;//yes this is odd. yes I spent hours trying to figure out why this wasnt working. yes, giving an extra 2 ticks of life allows particles to spawn. Id
     }
 
     @Override
@@ -112,15 +124,24 @@ public class BulletProjectileEntity extends AbstractArrow {
                 }
             }
         }
-        if (lifetime >= 40) {
+        if (lifetime >= maxLife) {
             this.discard();
-        } else if (this.isShotgun && lifetime == 10) {
+        } else if (this.isShotgun && lifetime >= maxShotgunLife) {
             this.discard();
         }
     }
 
     @Override
-    protected SoundEvent getDefaultHitGroundSoundEvent() {
-        return SoundEvents.BONE_BLOCK_HIT;
+        protected SoundEvent getDefaultHitGroundSoundEvent() {
+            return SoundEvents.BONE_BLOCK_HIT;
+    }
+        
+    private static int plusmin(){
+    if(Math.random()>0.5){
+            return -1;
+        }
+        else{
+            return 1;
+        }
     }
 }
