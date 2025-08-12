babce502bb7d8fe6bdf0d88aefe0cb9494143d63
ahkin <ahkingjasper@gmail.com>
2025-08-12 10:40:54 -0500

    Added extraction terminal structure, super sample structure, and basic poi structure and tweaked the worldgen for the Choepessa IV dimension

--- src/main/java/net/team/helldivers/worldgen/dimension/ModDimensions.java
+++ src/main/java/net/team/helldivers/worldgen/dimension/ModDimensions.java
@@ -83,7 +83,8 @@ public class ModDimensions {
 
         NoiseBasedChunkGenerator wrappedChunkGenerator = new NoiseBasedChunkGenerator(
                 new FixedBiomeSource(biomeRegistry.getOrThrow(ModBiomes.CHOEPESSA_WASTES)),
-                noiseGenSettings.getOrThrow(NoiseGeneratorSettings.OVERWORLD));
+                noiseGenSettings.getOrThrow(ResourceKey.create(Registries.NOISE_SETTINGS,
+                        ResourceLocation.fromNamespaceAndPath(HelldiversMod.MOD_ID, "choepessa_noise"))));
 
         NoiseBasedChunkGenerator noiseBasedChunkGenerator = new NoiseBasedChunkGenerator(
                 MultiNoiseBiomeSource.createFromList(
